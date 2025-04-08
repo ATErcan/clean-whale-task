8b073c0647a23e269a10b9735a4bfc7cadfaf96a
talha <atercan0658@gmail.com>
2025-04-08 12:41:03 +0200

    initialize express app

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const router = express.Router();
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use("/api", router);
+
+async function main() {
+  const dbURI = process.env.DB_URI;
+
+  if(!dbURI) {
+    console.error(
+      "Database URI is missing. Please set DB_URI in your .env file."
+    );
+    process.exit(1);
+  }
+
+  await mongoose.connect(dbURI);
+  app.listen(8080);
+}
+
+try {
+  main();
+} catch (error) {
+  console.error(error);
+}
+
+router.get("/home", (req, res) => res.send("Hello World!"));
