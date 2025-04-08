7e719262b47f1182c63ace372fbacc9d4f1c18e6
talha <atercan0658@gmail.com>
2025-04-08 14:00:45 +0200

    [feature/auth-signup] - create jwt token using jsonwebtoken library

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,8 +1,15 @@
+const { createUser } = require("../services/user-service");
+const { createToken } = require("../utils/auth-utils");
+
 const signup = async (req, res, next) => {
   const { email, password } = req.body;
 
   try {
-    
+    const user = await createUser({ email, password });
+    const token = createToken({
+      _id: user._id
+    })
+
   } catch (error) {
     
   }
