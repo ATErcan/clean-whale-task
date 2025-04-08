025832aa85395e7c03403c25ecb76de64506f6ec
A. Talha Ercan <109243387+ATErcan@users.noreply.github.com>
2025-04-08 12:58:07 +0200

    Merge pull request #1 from ATErcan/feature/user-model
    Feature - User Model

---
+++ backend/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      validate: [isEmail, "Invalid email address"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
