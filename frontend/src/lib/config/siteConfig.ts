1349c31026568de14106d2e8b1b291dc17508362
talha <atercan0658@gmail.com>
2025-04-09 22:21:32 +0200

    [feature/notifications] - update site metadata

---
+++ frontend/src/lib/config/siteConfig.ts
@@ -0,0 +1,6 @@
+import { Metadata } from "next";
+
+export const siteMetadata: Metadata = {
+  title: "Clean Whale Auth Task",
+  description: "A simple auth system task that has been given by CleanWhale",
+};
